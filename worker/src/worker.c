fcbe5e6b59a1e65a7f4376e027abc64612eefbc0
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-09-08 21:14:41 -0300

    Setup worker

---
+++ worker/src/worker.c
@@ -0,0 +1,14 @@
+/*
+ * worker.c
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Hernan Canzonetta
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Worker");
+
+	return EXIT_SUCCESS;
+}
