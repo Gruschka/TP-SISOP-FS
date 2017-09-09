e6ce8cf4240a3d6a8e21d63ed6be3a909516f1a5
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-09-08 21:12:58 -0300

    Setup master

---
+++ master/src/master.c
@@ -0,0 +1,15 @@
+/*
+ * master.c
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Hernan Canzonetta
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Master");
+
+	return EXIT_SUCCESS;
+}
+
