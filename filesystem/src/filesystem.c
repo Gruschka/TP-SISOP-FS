aaacacda78e1b2864bbb435e68e17f31117c5937
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-09-08 21:10:38 -0300

    Setup filesystem

---
+++ filesystem/src/filesystem.c
@@ -0,0 +1,14 @@
+/*
+ * filesystem.c
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Hernan Canzonetta
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Filesystem");
+
+	return EXIT_SUCCESS;
+}
