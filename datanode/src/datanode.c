64335bb7253741ac51b6624623c57129949e4cc0
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-09-08 21:12:04 -0300

    Setup datanode

---
+++ datanode/src/datanode.c
@@ -0,0 +1,16 @@
+/*
+ * datanode.c
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Hernan Canzonetta
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("DataNode");
+
+	return EXIT_SUCCESS;
+}
+
+
