6d16baf067d45114ca502ea05662438886163baf
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-09-08 21:06:15 -0300

    Setup yama

---
+++ yama/src/yama.c
@@ -0,0 +1,14 @@
+/*
+ * yama.c
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Hernan Canzonetta
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Yama");
+
+	return EXIT_SUCCESS;
+}
