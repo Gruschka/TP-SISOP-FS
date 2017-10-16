9f7810d5bb9c76b8e8c808387824193d0e3f4914
German Gruschka <gegruschka@gmail.com>
2017-10-16 20:20:29 -0300

    [DataNode] Base para getNode y setNode

--- node/datanode/src/datanode.c
+++ node/datanode/src/datanode.c
@@ -1,7 +1,7 @@
 /*TODO: Levantar info de bloques libres del bitmap
-TODO: Setear bien cantidad de nodos libres
+ TODO: Setear bien cantidad de nodos libres
 
-*/
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "datanode.h"
@@ -16,7 +16,6 @@ t_dataNode myDataNode;
 t_dataNodeConfig nodeConfig;
 
 int main(int argc, char **argv) {
-	FILE * dataBinFile;
 
 	char *logFile = tmpnam(NULL);
 
@@ -33,12 +32,17 @@ int main(int argc, char **argv) {
 	}
 
 	//Abro el archivo data.bin y si no lo creo con el tamanio pasado por config
-	dataBinFile = dataNode_openOrCreateDataBinFile(
+	int dataBinResult = dataNode_openOrCreateDataBinFile(
 			myDataNode.config.databinPath, myDataNode.config.sizeInMb);
 
-	dataNode_setBlockInformation(&myDataNode);
+	FILE * dataBinFile = fopen(myDataNode.config.databinPath,"w+");
 
-	//Me conecto al FS
+	dataNode_setBlockInformation(&myDataNode);
+	char *test = "ESCRIBIESTOCONLAFUNCION";
+	dataNode_setBlock(0,test);
+	void *prueba = malloc(BLOCK_SIZE);
+	memset(prueba, 0, BLOCK_SIZE);
+	prueba = dataNode_getBlock(0);
 
 	dataNode_connectToFileSystem(myDataNode);
 
@@ -63,13 +67,13 @@ int dataNode_loadConfig(t_dataNode *dataNode) {
 
 }
 
-FILE *dataNode_openOrCreateDataBinFile(char *dataBinPath, int sizeInMb) {
+int dataNode_openOrCreateDataBinFile(char *dataBinPath, int sizeInMb) {
 
 	FILE * dataBinFileDescriptor;
 
 	if (dataBinFileDescriptor = fopen(dataBinPath, "r+")) { //Existe el archivo de data.bin
-		printf("Existe el data.bin");
-		return dataBinFileDescriptor;
+		log_debug(logger, "Data.bin file  found. Wont create from scratch");
+		return EXIT_FAILURE;
 
 	} else { //No puede abrirlo => Lo crea
 		printf("NoExiste el data.bin");
@@ -77,8 +81,8 @@ FILE *dataNode_openOrCreateDataBinFile(char *dataBinPath, int sizeInMb) {
 		log_debug(logger,
 				"Data.bin file not found. Creating with parameters of config file");
 		dataBinFileDescriptor = fopen(dataBinPath, "w+");
-		ftruncate(fileno(dataBinFileDescriptor), sizeInMb * 1024 * 1024);
-		return dataBinFileDescriptor;
+		ftruncate(fileno(dataBinFileDescriptor), sizeInMb * BLOCK_SIZE);
+		return EXIT_SUCCESS;
 	}
 
 	return NULL;
@@ -114,7 +118,6 @@ void dataNode_connectToFileSystem(t_dataNode dataNode) {
 		exit(1);
 	}
 
-
 	char buffer[1024] = { 0 };
 
 	//Send block name
@@ -123,7 +126,7 @@ void dataNode_connectToFileSystem(t_dataNode dataNode) {
 
 	//Send amount of blocks
 	int myInt = dataNode.blockInfo.amountOfBlocks;
-		int tmp = htonl((uint32_t) myInt);
+	int tmp = htonl((uint32_t) myInt);
 
 	write(sockfd, &tmp, sizeof(tmp));
 
@@ -139,10 +142,9 @@ void dataNode_connectToFileSystem(t_dataNode dataNode) {
 
 	printf("%s\n", buffer);
 
-
 	//wait for request from fs
 
-	while(1){
+	while (1) {
 
 		//Codigo para escuchar pedidos del fs aca;lk;lk
 		//printf("DataNode %s esperando pedidos del FS\n",dataNode.config.nodeName);
@@ -159,5 +161,70 @@ void dataNode_setBlockInformation(t_dataNode *aDataNode) {
 	aDataNode->blockInfo.freeBlocks = 5;
 	aDataNode->blockInfo.occupiedBlocks = aDataNode->blockInfo.amountOfBlocks
 			- aDataNode->blockInfo.freeBlocks;
+
+}
+
+void *dataNode_getBlock(int blockNumber) {
+
+	FILE * dataBinFileDescriptor;
+	int result = 0;
+	void *blockInformation = malloc(BLOCK_SIZE);
+	memset(blockInformation, 0, BLOCK_SIZE);
+
+	dataBinFileDescriptor = fopen(myDataNode.config.databinPath, "r+"); //Abro el archivo
+
+	int positionInBytesOfTheBlock = blockNumber * BLOCK_SIZE;
+
+	//Me posiciono en el bloque correspondiente partiendo desde el principio
+	result = fseek(dataBinFileDescriptor, positionInBytesOfTheBlock, SEEK_SET);
+
+	if(result == -1){
+			log_error(logger, "Error al intentar posicionar el bloque %d del databin",blockNumber);
+			return NULL;
+		}
+
+	//Leo 1 BLOCK_SIZE (1024*1024)
+	result = fread(blockInformation, 1, BLOCK_SIZE, dataBinFileDescriptor);
+
+	if(result == 0){
+		log_error(logger, "Error al intentar leer bloque %d",blockNumber);
+		return NULL;
+	}
+
+	fclose(dataBinFileDescriptor);
+
+	return blockInformation;
+
+}
+
+int dataNode_setBlock(int blockNumber, void *dataToWrite) {
+
+	FILE * dataBinFileDescriptor;
+	int result = 0;
+
+	//Abro el archivo
+	dataBinFileDescriptor = fopen(myDataNode.config.databinPath, "w+");
+
+	int positionInBytesOfTheBlock = blockNumber * BLOCK_SIZE;
+
+	//Me posiciono en el bloque correspondiente partiendo desde el principio
+	result = fseek(dataBinFileDescriptor, positionInBytesOfTheBlock, SEEK_SET);
+
+	if(result == -1){
+			log_error(logger, "Error al intentar posicionar el bloque %d del databin",blockNumber);
+			return EXIT_FAILURE;
+		}
+
+	//Leo 1 BLOCK_SIZE (1024*1024)
+	result = fwrite(dataToWrite, 1, BLOCK_SIZE, dataBinFileDescriptor);
+
+	if(result == 0){
+		log_error(logger, "Error al intentar leer bloque %d",blockNumber);
+		return EXIT_FAILURE;
+	}
+
+	fclose(dataBinFileDescriptor);
+
+	return EXIT_SUCCESS;
 
 }
