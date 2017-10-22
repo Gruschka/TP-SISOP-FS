ca38be081549995e4012bc785a7053454999b032
Federico Trimboli <federicotrimboli@gmail.com>
2017-10-22 18:43:50 -0300

    conflicts: master

--- master/src/master.c
+++ master/src/master.c
@@ -9,11 +9,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string.h>
 #include <ipc/ipc.h>
 #include <ipc/serialization.h>
 
-<<<<<<< HEAD
 int main(int argc, char **argv) {
 	if (argc != 5) {
 		printf("El proceso master debe recibir 4 argumentos.");
@@ -25,15 +25,12 @@ int main(int argc, char **argv) {
 	char *inputFilePath = argv[3];
 	char *outputFilePath = argv[4];
 
-=======
-int main() {
-	printf("Master");
+	// TODO: (Fede) el siguiente es código de Hernie. Revisar.
 	initialize();
 	int sockfd = ipc_createAndConnect(8888, "127.0.0.1");
 	ipc_struct_test_message testMessage;
 	testMessage.blah = 'A';
 	testMessage.bleh = strdup("QTI AMIGO");
 	ipc_sendMessage(sockfd, TEST_MESSAGE, &testMessage);
->>>>>>> branch 'master' of git@github.com:sisoputnfrba/tp-2017-2c-Deus-Vult.git
 	return EXIT_SUCCESS;
 }
