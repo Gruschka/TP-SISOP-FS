dec747defe60737850327f6ff9975b294a4ff1b0
Federico Trimboli <federicotrimboli@gmail.com>
2017-10-22 18:37:24 -0300

    [master] avances

--- master/src/master.c
+++ master/src/master.c
@@ -10,9 +10,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	printf("Master");
+int main(int argc, char **argv) {
+	if (argc != 5) {
+		printf("El proceso master debe recibir 4 argumentos.");
+		return EXIT_FAILURE;
+	}
+
+	char *transformScriptPath = argv[1];
+	char *reduceScriptPath = argv[2];
+	char *inputFilePath = argv[3];
+	char *outputFilePath = argv[4];
 
 	return EXIT_SUCCESS;
 }
-
