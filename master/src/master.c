f9a8f5c65a2aec633555c9e4c496b92fd483fc57
Federico Trimboli <federicotrimboli@gmail.com>
2017-11-01 23:45:53 -0300

    [master] agregar TODOs

--- master/src/master.c
+++ master/src/master.c
@@ -9,15 +9,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
 
 #include <string.h>
 #include <ipc/ipc.h>
 #include <ipc/serialization.h>
 
-int main(int argc, char **argv) {
-	FILE *file = fopen("./resultado.txt", "w");
-	fputs("ok", file);
+// Setup
+// TODO: al iniciar, comunicarse con YAMA e indicarle el archivo
+// sobre el cual quiero operar.
+//
+// Etapa de transformación
+// TODO: recibir de YAMA qué procesos worker conectarme (IP y puerto)
+// qué bloque de cada uno de ellos se debe aplicar transformación,
+// y nombre del archivo temporal donde se debe almacenar el resultado.
+// Por cada tarea de transformación:
+// 1. TODO: crear un hilo
+// 2. TODO: conectarse al worker correspondiente
+// 3. TODO: enviarle el programa de transformación
+// 4. TODO: indicarle sobre qué bloque debe ejecutar el programa, la cantidad
+//    de bytes ocupados en dicho bloque, y el archivo temporal donde
+//    guardar el resultado.
+// 5. TODO: esperar confirmación de cada etapa y comunicar a YAMA el resultado
+//
+// Etapa de reducción local
+// TODO: esperar indicación de YAMA con el nombre de los archivos
+// temporales por cada nodo sobre el cual se deba aplicar reducción
+// y el nombre del archivo temporal resultado de la reducción.
+// Por cada nodo:
+// 1. TODO: crear un hilo
+// 2. TOOD: conectarse al worker
+// 3. TODO: enviarle el programa de reducción, la lista de archivos
+//    temporales del nodo y el nombre del temporal resultante.
+// 4. TODO: esperar confirmación del worker y notificar resultado
+//    a YAMA.
+//
+// Etapa de reducción global
+// TODO: recibir de YAMA la IP y puerto del worker "encargado",
+// el nombre del archivo temporal de reducción de cada worker,
+// el nombre del archivo temporal donde guardar el resultado.
+// TODO: conectarse al worker encargado
+// TODO: enviarle el programa de reducción, lista de todos los
+// workers con sus puertos e IPs, y los nombres de los temporales
+// de reducción local.
+// TODO: esperar confirmación del worker encargado y notificar
+// resultado a YAMA.
+//
+// Almacenado final
+// TODO: recibir de YAMA la IP y puerto del worker "encargado",
+// y el nombre del archivo resultado de la reducción global.
+// TODO: conectarse al worker y pedirle que le envíe al FS
+// TODO: el archivo resultado de la reducción global
+// y el nombre y path bajo el cual deberá almacenarse.
+// TODO: esperar confirmación y notificar a YAMA.
+//
 
+int main(int argc, char **argv) {
 	if (argc != 5) {
 		printf("El proceso master debe recibir 4 argumentos.");
 		return EXIT_FAILURE;
@@ -28,12 +75,17 @@ int main(int argc, char **argv) {
 	char *inputFilePath = argv[3];
 	char *outputFilePath = argv[4];
 
+	t_config *config = config_create("conf/master.conf");
+	int yamaPort = config_get_int_value(config, "YAMA_PUERTO");
+	char *yamaIP = config_get_string_value(config, "YAMA_IP");
+	config_destroy(config);
+
 	// TODO: (Fede) el siguiente es código de Hernie. Revisar.
-	initialize();
-	int sockfd = ipc_createAndConnect(8888, "127.0.0.1");
-	ipc_struct_test_message testMessage;
-	testMessage.blah = 'A';
-	testMessage.bleh = strdup("QTI AMIGO");
-	ipc_sendMessage(sockfd, TEST_MESSAGE, &testMessage);
+//	initialize();
+//	int sockfd = ipc_createAndConnect(yamaPort, yamaIP);
+//	ipc_struct_test_message testMessage;
+//	testMessage.blah = 'A';
+//	testMessage.bleh = strdup("QTI AMIGO");
+//	ipc_sendMessage(sockfd, TEST_MESSAGE, &testMessage);
 	return EXIT_SUCCESS;
 }
