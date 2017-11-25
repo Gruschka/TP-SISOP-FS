6705c124ad28d643905b58668d8365c44f0d07b7
Hernan Canzonetta <hcanzonetta@gmail.com>
2017-11-25 16:41:27 -0300

    fix

--- yama/src/yama.c
+++ yama/src/yama.c
@@ -175,16 +175,29 @@ ipc_struct_fs_get_file_info_response *requestInfoToFilesystem(char *filePath) {
 	request->filePath = filePath;
 	ipc_sendMessage(fsFd, FS_GET_FILE_INFO_REQUEST, request);
 	ipc_struct_fs_get_file_info_response *response = ipc_recvMessage(fsFd, FS_GET_FILE_INFO_RESPONSE);
-	printf("File %s has %d blocks", response->entriesCount);
+	printf("File %s has %d blocks", filePath, response->entriesCount);
 
 	int i;
 	for (i = 0; i < response->entriesCount; i++) {
 		ipc_struct_fs_get_file_info_response_entry *entry = response->entries + i;
-		WorkerInfo *workerInfo = malloc(sizeof(WorkerInfo));
-		workerInfo->id = strdup(entry->firstCopyNodeID);
-		workerInfo->ip = strdup(entry->firstCopyNodeIP);
-		workerInfo->port = entry->firstCopyNodePort;
-		dictionary_put(workersDict, entry->firstCopyNodeID, workerInfo);
+		WorkerInfo *firstWorkerInfo = malloc(sizeof(WorkerInfo));
+		firstWorkerInfo->id = strdup(entry->firstCopyNodeID);
+		firstWorkerInfo->ip = strdup(entry->firstCopyNodeIP);
+		firstWorkerInfo->port = entry->firstCopyNodePort;
+		log_debug(logger, "firstWorkerInfo: %s", entry->firstCopyNodeID);
+		//TODO: ver como serializar/deserializar cuando no esta alguna copia
+		if (!dictionary_has_key(workersDict, entry->firstCopyNodeID)) {
+			dictionary_put(workersDict, entry->firstCopyNodeID, firstWorkerInfo);
+		}
+
+		WorkerInfo *secondWorkerInfo = malloc(sizeof(WorkerInfo));
+		secondWorkerInfo->id = strdup(entry->secondCopyNodeID);
+		secondWorkerInfo->ip = strdup(entry->secondCopyNodeIP);
+		secondWorkerInfo->port = entry->secondCopyNodePort;
+		log_debug(logger, "secondWorkerInfo: %s", entry->secondCopyNodeID);
+		if (!dictionary_has_key(workersDict, entry->secondCopyNodeID)) {
+			dictionary_put(workersDict, entry->secondCopyNodeID, secondWorkerInfo);
+		}
 	}
 	return response;
 }
